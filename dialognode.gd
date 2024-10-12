db4b7160dff1382e2942efba0db62c36410b703f
m <m@mcarr.one>
2024-10-11 23:46:18 -0700

    git push

--- dialognode.gd
+++ dialognode.gd
@@ -3,6 +3,11 @@ extends DialogTree
 
 @export var following: DialogTree
 
+func _init(text: String, following: DialogTree, mutate_player):
+	self.text = text
+	self.following = following
+	self.mutate_player = mutate_player
+
 func _next(data: int) -> DialogTree:
 	return following
 	
