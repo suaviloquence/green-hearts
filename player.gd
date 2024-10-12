cdd603c390cc02937f08296780829271c568e886
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2024-10-11 22:24:36 -0700

    signals stuff iguess

---
+++ player.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+@export var name: String
+# pronouns and anything else player needs to do
--- story_box_choose_3.gd
+++ story_box_choose_3.gd
@@ -7,7 +7,15 @@ func _on_done() -> void:
 	($Area2D/TextureRect/MarginContainer as MarginContainer).add_theme_constant_override("margin_right", 384)
 	$Area2D/Options.show()
 
-
 func _on_option_1_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("select"):
 		chose.emit(1)
+
+func _on_option_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("select"):
+		chose.emit(2)
+
+
+func _on_option_3_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("select"):
+		chose.emit(3)
