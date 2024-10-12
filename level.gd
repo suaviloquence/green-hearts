566861f26cc0acd307dbb70ee495a870e8106b55
m <m@mcarr.one>
2024-10-12 12:39:49 -0700

    fixed branch

--- level.gd
+++ level.gd
@@ -7,41 +7,42 @@ signal done
 
 @onready var player := $Player as Player
 
-
-func _ready() -> void:
+func _ready():
 	render_tree(tree)
 
 func render_tree(dtree: DialogTree):
-	if dtree.mutate_player:
-		dtree.mutate_player.call(player)
-	var data := 0
-	if dtree is DialogNode:
-		var node := preload("res://story_box_continue.tscn").instantiate()
-		node.text = render(dtree.text)
-		add_box(node)
-		await node.really_done
-		node.queue_free()
-	elif dtree is Choose2:
-		var node := preload("res://story_box_choose2.tscn").instantiate()
-		node.text = render(dtree.text)
-		node.first_text = render(dtree.first_text)
-		node.second_text = render(dtree.second_text)
-		add_box(node)
-		data = await node.chose
-		node.queue_free()
-	elif dtree is Choose3:
-		var node := preload("res://story_box_choose3.tscn").instantiate()
-		node.text = render(dtree.text)
-		node.first_text = render(dtree.first_text)
-		node.second_text = render(dtree.second_text)
-		node.third_text = render(dtree.third_text)
-		add_box(node)
-		data = await node.chose
-		node.queue_free()
-	elif dtree is Terminus:
-		done.emit()
-	dtree = dtree.next(data)
-	render_tree(dtree)
+	while dtree and dtree is not Terminus:
+		if dtree and dtree.mutate_player:
+			dtree.mutate_player.call(player)
+		var data := 0
+		if dtree is DialogNode:
+			var node := preload("res://story_box_continue.tscn").instantiate()
+			node.text = render(dtree.text)
+			add_box(node)
+			await node.really_done
+			node.queue_free()
+		elif dtree is Choose2:
+			var node := preload("res://story_box_choose2.tscn").instantiate()
+			node.text = render(dtree.text)
+			node.first_text = render(dtree.first_text)
+			node.second_text = render(dtree.second_text)
+			add_box(node)
+			data = await node.chose
+			node.queue_free()
+		elif dtree is Choose3:
+			var node := preload("res://story_box_choose3.tscn").instantiate()
+			node.text = render(dtree.text)
+			node.first_text = render(dtree.first_text)
+			node.second_text = render(dtree.second_text)
+			node.third_text = render(dtree.third_text)
+			add_box(node)
+			data = await node.chose
+			node.queue_free()
+		
+		dtree = dtree.next(data)
+	
+	done.emit()
+	self.queue_free()
 
 func add_box(node: StoryBox):
 	node.position = Vector2(768/2, 512-128/2)
--- player.gd
+++ player.gd
@@ -2,14 +2,14 @@ extends Node2D
 class_name Player
 
 # name
-@export var player_name: String
+@export var player_name: String = ""
 # pronouns
-@export var they: String
-@export var them: String
-@export var their: String
-@export var theirs: String
-@export var themself: String
+@export var they := "they"
+@export var them := "them"
+@export var their := "their"
+@export var theirs := "theirs"
+@export var themself := "themself"
 # pronouns and anything else player needs to do
 
-@export var affection_meter: int
-@export var bad_decisions: Array
+@export var affection_meter: int = 0
+@export var bad_decisions: Array = []
