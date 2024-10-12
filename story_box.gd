2f8f098d886c3430d69a95b748ca52e026646de8
m <m@mcarr.one>
2024-10-11 21:52:20 -0700

    text box 1

---
+++ story_box.gd
@@ -0,0 +1,35 @@
+class_name StoryBox
+extends Node2D
+
+@export_multiline var text: String
+@export var speed := 3.0
+
+@onready var label := $Area2D/TextureRect/MarginContainer/RichTextLabel as RichTextLabel
+
+signal done
+
+var speed_mult := 1.0
+
+func _ready():
+	start()
+
+func start():
+	var lines := text.split("\n")
+	label.text = ""
+	label.scroll_following = true
+	
+	for line in lines:
+		var words := line.split(" ")
+		for word in words:
+			label.append_text(word + " ")
+			await get_tree().create_timer(1 / (speed * speed_mult)).timeout
+		label.append_text("\n")
+		await get_tree().create_timer(1 / (speed)).timeout
+	
+	done.emit()
+		
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("fast_forward"):
+		speed_mult *= 5
