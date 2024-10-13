ff77fb2af382b040b29dcb8aea78d51c8e855bc6
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2024-10-12 21:08:18 -0700

    freaking pushing them

---
+++ nile_confession.gd
@@ -0,0 +1,36 @@
+extends Level
+
+
+var ultimatum_start = DialogNode.new("Nile frowns, a little shocked by your statement.
+An ultimatum, huh? I get that this matters to you, but I don’t know if I can just flip a switch like that. I don’t like being told how to live my life, even if your reasons are good.", null, null, 
+	func(player):
+		player.hearts -= 1
+		explode("💔")
+)
+
+var try_start = DialogNode.new("Nile: I… I get it. I’ve never really thought about this stuff like you do, but I can see how much it means to you. If it’s that important to you, then yeah—I’ll try. I’m not making any promises to go zero waste tomorrow, but I’ll give it a shot.", null)
+
+#need to make the choices
+var confession_5 = Choose2.new("$name: Nile, I really like you. But there’s something I need to tell you. Sustainability isn’t just something I care about—it’s a core part of who I am. I want to build something real between us, 
+but... I don’t think I can keep going if we’re not aligned on this. I need to know that you care about this planet as much as I do.", 
+try_start, "I don’t expect you to change everything overnight. But I need to know that you’re willing to try.", ultimatum_start, "If you can’t make that change, then I don’t think I can continue this relationship")
+var confession_4 = DialogNode.new("The sun is starting to set, painting the sky with watercolors of orange and pink. It’s the perfect time to confess.", confession_5)
+var confession_3 = DialogNode.new("Nile looks at you with a soft gaze. 
+Nile: I feel the same with you. It’s like everything around me disappears when I’m with you.", confession_4)
+var confession_2 = DialogNode.new("$name: I really enjoy my time with you Nile, I feel so calm and free.", confession_3)
+var confession_1 = DialogNode.new("You and Nile are sitting close to each other on the beach, hands almost touching. After a fun day running around the beach together, the atmosphere got tense. It’s like Nile is also anticipating a serious conversation.", confession_2)
+var confession_intro = DialogNode.new("It has been a couple of dates since the cooking one. It’s enough for you to feel like it’s time to confess to Nile, 
+and see if he’s willing to be sustainable with you. You have invited him on a beach date, with a little romantic picnic! 
+Remember, tread carefully, if he doesn’t choose to live sustainably after this confession, it may be impossible to ever control the water damage he can cause. ", confession_1)
+
+var ending = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.tree = confession_1
+	super._ready()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ nile_endings.gd
@@ -0,0 +1,25 @@
+extends Level
+
+var bad_ending_final = DialogNode.new("**BREAKING NEWS: HOUSES ALONG THE COAST DESTROYED DUE TO FLASH FLOOD**", null)
+var bad_ending_6 = DialogNode.new("You can only watch as the tide rushes towards you. You failed.", bad_ending_final)
+var bad_ending_5 = DialogNode.new("He walks away as the ocean tide gets higher and higher. The lifeguard on the beach is yelling in her megaphone to run, that something isn’t right. That this is unnatural.", bad_ending_6)
+var bad_ending_4 = DialogNode.new("Nile: You’re just like the others. Just want to convert me for your own purpose. I’m done.", bad_ending_5)
+var bad_ending_3 = DialogNode.new("You can’t say a word. He figured out your mission himself. He notices your silence, and sighs.", bad_ending_4)
+var bad_ending_2 = DialogNode.new("He takes a pause, before his expression turns angry.
+Nile: Wait, every time we were together, you mentioned sustainability. Why did you mention it so much? Were you pushing that agenda on me?", bad_ending_3)
+var bad_ending_1 = DialogNode.new("Nile: As much as I like you $name, I’m sorry. I don’t think I can do this. Maybe we’re just too different.", bad_ending_2)
+var bad_ending_start = DialogNode.new("Nile stands up, looking towards the ocean. He doesn’t look down at you at all. His tone is distant, and the playful warmth from earlier has vanished.", bad_ending_1)
+
+var good_ending_final = DialogNode.new("Fin.", null)
+var good_ending_4 = DialogNode.new("You both sit back down and continue to watch the sunset, holding hands as the tide seems calmer than ever.", good_ending_final)
+var good_ending_3 = DialogNode.new("Nile: $name, I’m not passing you up for anything.", good_ending_4)
+var good_ending_2 = DialogNode.new("$name: Of course, I’m glad that you’re willing to make this work. It means a lot to me.", good_ending_3)
+var good_ending_1 = DialogNode.new("Nile: “You’re right. I  want this to work, and I’m willing to learn. Maybe you can teach me more about sustainability over time?", good_ending_2)
+var good_ending_start = DialogNode.new("Nile stands up, and then reaches a hand out to you. He pulls you into a warm embrace, voice soft.", good_ending_1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if player.hearts >= 3:
+		self.tree = good_ending_start
+	else:
+		self.tree = bad_ending_start # Replace with function body.
